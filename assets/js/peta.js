887a66cc4d86ebe54754f006d236939918a63ba1
cakra69 <cakrabayuraharjdo@gmail.com>
2024-08-07 15:14:08 +0700

    cc

--- assets/js/peta.js
+++ assets/js/peta.js
@@ -207,8 +207,8 @@ L.LogoControl = L.Control.extend({
     container.title = "Compass";
 
     // Adjust the position slightly
-    container.style.marginBottom = "100px";
-    container.style.marginRight = "50px";
+    container.style.marginBottom = "50px";
+    container.style.marginRight = "100px";
 
     return container;
   },
