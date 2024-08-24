2cfeb5f52f3cd1159caa5c6dbf305e4065d78c80
cakra69 <cakrabayuraharjdo@gmail.com>
2024-08-25 02:37:47 +0700

    new update 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,6 @@
-/**
-* Template Name: Logis
-* Template URL: https://bootstrapmade.com/logis-bootstrap-logistics-website-template/
-* Updated: Jun 29 2024 with Bootstrap v5.3.3
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
-
 (function() {
   "use strict";
 
-  /**
-   * Apply .scrolled class to the body as the page is scrolled down
-   */
   function toggleScrolled() {
     const selectBody = document.querySelector('body');
     const selectHeader = document.querySelector('#header');
